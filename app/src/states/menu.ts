71dcdd726a43ddcf952891b493f4ce40660c63cb
hanselrd <hansel809@hotmail.com>
2017-05-01 22:22:02 -0400

    Removed redundant 'game' prefix and added a button

--- app/src/states/menu.ts
+++ app/src/states/menu.ts
@@ -1,22 +1,27 @@
 export class Menu extends Phaser.State {
     private playKey: Phaser.Key;
+    private playButton: Phaser.Button;
 
     public preload() {
-        this.game.load.image("background", require("../../assets/backgrounds/bg-800x600.png"));
+        this.load.image("background", require("../../assets/backgrounds/bg-800x600.png"));
     }
 
     public create() {
-        this.playKey = this.game.input.keyboard.addKey(Phaser.KeyCode.ENTER);
-        this.game.add.sprite(0, 0, "background");
-        this.game.add.text(30, 30, "Hit [ENTER] to Play!", {
+        this.playKey = this.input.keyboard.addKey(Phaser.KeyCode.ENTER);
+        this.add.sprite(0, 0, "background");
+        this.add.text(30, 30, "Hit [ENTER] to Play!", {
             fill: "#0f0",
             fontSize: 32,
         });
+        this.playButton = this.add.button(30, 30, "k", () => {
+            this.state.start("play");
+        });
+        //this.playButton.fixedToCamera = true;
     }
 
     public update() {
         if (this.playKey.isDown) {
-            this.game.state.start("play");
+            this.state.start("play");
         }
     }
 }
--- app/src/states/play.ts
+++ app/src/states/play.ts
@@ -6,41 +6,41 @@ export class Play extends Phaser.State {
     private trees: Phaser.Group;
 
     public preload() {
-        this.game.load.tilemap("world", require("../../assets/maps/world.json"), null, Phaser.Tilemap.TILED_JSON);
-        this.game.load.spritesheet("Base", require("../../assets/tilesets/base.png"), 16, 16, -1, 0, 1);
-        this.game.load.spritesheet("City", require("../../assets/tilesets/city.png"), 16, 16, -1, 0, 1);
-        this.game.load.spritesheet("dude", require("../../assets/spritesheets/dude.png"), 32, 48);
+        this.load.tilemap("world", require("../../assets/maps/world.json"), null, Phaser.Tilemap.TILED_JSON);
+        this.load.spritesheet("Base", require("../../assets/tilesets/base.png"), 16, 16, -1, 0, 1);
+        this.load.spritesheet("City", require("../../assets/tilesets/city.png"), 16, 16, -1, 0, 1);
+        this.load.spritesheet("dude", require("../../assets/spritesheets/dude.png"), 32, 48);
     }
 
     public create() {
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+        this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.map = this.game.add.tilemap("world");
+        this.map = this.add.tilemap("world");
         this.map.addTilesetImage("Base");
         this.map.addTilesetImage("City");
         this.map.createLayer("Background").resizeWorld();
 
-        this.player = this.game.add.sprite(100, 300, "dude", 4);
+        this.player = this.add.sprite(100, 300, "dude", 4);
         this.player.anchor.setTo(0.5);
         this.player.animations.add("left", [0, 1, 2, 3], 7, true);
         this.player.animations.add("right", [5, 6, 7, 8], 7, true);
-        this.game.physics.enable(this.player);
+        this.physics.enable(this.player);
         this.player.body.collideWorldBounds = true;
         this.camera.follow(this.player);
 
-        this.trees = this.game.add.group();
+        this.trees = this.add.group();
         this.trees.enableBody = true;
         this.map.createFromObjects("Trees", 646, "Base", 645, true, true, this.trees);
         this.trees.setAll("body.immovable", true);
         this.map.createLayer("Foreground");
 
-        this.coordsText = this.game.add.text(0, 0, "");
+        this.coordsText = this.add.text(0, 0, "");
         this.coordsText.fixedToCamera = true;
     }
 
     public update() {
-        this.game.physics.arcade.collide(this.player, this.trees);
+        this.physics.arcade.collide(this.player, this.trees);
 
         this.player.body.velocity.x = 0;
         this.player.body.velocity.y = 0;
