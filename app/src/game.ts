0d999d391f65577b720aeddd8b1f2a33bd58df66
hanselrd <hansel809@hotmail.com>
2017-05-02 00:49:05 -0400

    Made game have dynamic window size

--- app/src/game.ts
+++ app/src/game.ts
@@ -5,7 +5,7 @@ export class Game {
     private game: Phaser.Game;
 
     constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, "game");
+        this.game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * devicePixelRatio, Phaser.AUTO, "game");
         this.game.state.add("menu", new Menu());
         this.game.state.add("play", new Play());
         this.game.state.start("menu");
